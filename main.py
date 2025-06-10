df59b9a7b5632bb87aba52d8faf16c3966387e28
gutierrez-kingandrei <kingandreibgutierrez@iskolarngbayan.pup.edu.ph>
2025-06-10 15:02:02 +0800

    Add and Initilize main.py

---
+++ main.py
@@ -0,0 +1,38 @@
+#Import modules from pyqtfive package
+#TODO (ARGUELLES): Import module from pyqtfive package
+#TODO (CAYA): Import module from pyqtfive package
+#TODO (CONDINO): Import module from pyqtfive package
+#TODO (CORDOVA): Import module from pyqtfive package
+#TODO (GUTIERREZ): Import module from pyqtfive package
+
+while True:
+    print("=== Team Member Menu ===")
+    print("1 - Norlan Arguelles")
+    print("2 - Karl Caya")
+    print("3 - Ciara Marie Condino")
+    print("4 - Aron Stephen Cordova")
+    print("5 - King Andrei Gutierrez")
+    print("6 - Exit")
+    
+    choice = input("\nSelect a team member: ")
+    
+    match choice:
+        case "1":
+            #TODO (ARGUELLES): Call the appropriate function here
+            pass
+        case "2":
+            #TODO (CAYA): Call the appropriate function here
+            pass 
+        case "3":
+            #TODO (CONDINO): Call the appropriate function here
+            pass
+        case "4":
+            #TODO (CORDOVA): Call the appropriate function here
+            pass 
+        case "5":
+            #TODO (GUTIERREZ): Call the appropriate function here
+            pass
+        case "6":
+            break   
+        case _:
+            print("Invalid choice. Please try again.")
